569c1b02f47dfae65d59539f1ba6cf3cea29557a
nickdu088 <26584779+nickdu088@users.noreply.github.com>
2020-09-10 20:31:19 +1000

    Initial version

---
+++ Assets/Scripts/QRScanner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using ZXing;
+
+public class QRScanner : MonoBehaviour
+{
+    WebCamTexture webcamTexture;
+    string QrCode = string.Empty;
+
+    void Start()
+    {
+        var renderer = GetComponent<RawImage>();
+        webcamTexture = new WebCamTexture(512, 512);
+        renderer.material.mainTexture = webcamTexture;
+        StartCoroutine(GetQRCode());
+    }
+
+    IEnumerator GetQRCode()
+    {
+        IBarcodeReader barCodeReader = new BarcodeReader();
+        webcamTexture.Play();
+        var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+        while (string.IsNullOrEmpty(QrCode))
+        {
+            try
+            {
+                snap.SetPixels32(webcamTexture.GetPixels32());
+                var Result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+                if (Result != null)
+                {
+                    QrCode = Result.Text;
+                    if (!string.IsNullOrEmpty(QrCode))
+                    {
+                        Debug.Log("DECODED TEXT FROM QR: " + QrCode);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex) { Debug.LogWarning(ex.Message); }
+            yield return null;
+        }
+        webcamTexture.Stop();
+    }
+}
